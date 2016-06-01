5604c9db5c6fdc1edffdc98abec1a25309a34e21
Stefanie Centi <stefcenti@centiumtech.com>
2016-06-01 00:30:36 -0400

    Added functionality to allow user to start the game and count down to 0.  To Do: Add Trivia and allow user to choose an answer; Tally the correct/incorrect answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,135 @@
+
+var trivia = {
+  gameTimeMinutes: 0.25, // start with 1 minute to answer questions
+  gameTime: 0,   // amount of time in seconds
+  timeLeft: 0,   // amount of time left in seconds
+  interval: 1,   // interval for updating the time on the screen in seconds
+  intervalId: -1, //
+  
+  incorrectAnswers: 0,
+  correctAnswers: 0,
+
+  oneMinute: 60, // number of seconds in a minute
+
+  questions: [
+    {
+      question: 'What color is the sky',
+      choices: ['blue', 'red', 'green'],
+      answer: 'blue'
+    },
+    {
+      question: 'How many months are in a year',
+      choices: [11, 12, 13],
+      answer: 12
+    }
+  ],
+
+  initGame: function() {
+    // Initialize variables and set up a timer to start the game
+    this.gameTime = this.gameTimeMinutes * this.oneMinute;
+    this.timeLeft = this.gameTime; // This may not be needed
+
+    $("#time-left").text(this.timeConverter(this.gameTime));
+
+  },
+
+  showQuestion: function() {
+
+  },
+
+  // The user started the game, set our timer and interval.
+  start: function() {
+    // The global timer functions take time in milliseconds.
+    // Since we are using seconds for our game, mult. by 1000.
+    setTimeout(gameOver, this.gameTime * 1000);
+
+    //  Use setInterval to call the timeLeft() method to update the timeLeft element
+    this.intervalId = setInterval(setTimeLeft, this.interval * 1000);
+  },
+
+  // Takes in time (t) in seconds and converts to
+  // a string minutes:seconds like so 00:00
+  timeConverter: function(t){
+    //This function is done. You do not need to touch it. It takes the current time in seconds and converts it to minutes and seconds (mm:ss).
+    var minutes = Math.floor(t/this.oneMinute);
+    var seconds = t - (minutes * this.oneMinute);
+    if (seconds < 10){
+      seconds = "0" + seconds;
+    }
+    if (minutes === 0){
+      minutes = "00";
+    } else if (minutes < 10){
+      minutes = "0" + minutes;
+    }
+    return minutes + ":" + seconds;
+  },
+
+  setTimeLeft: function() {
+    // Update time-left element
+    this.timeLeft -= this.interval;
+    $("#time-left").text(this.timeConverter(this.timeLeft));
+
+    if (this.timeLeft <= 0) {
+      clearInterval(this.intervalId);
+    }
+
+    // Set the number of valid and invalid counts in the html
+  },
+
+  gameOver: function() {
+    alert("Game Over!! Correct Answers: " + this.correctAnswers + 
+          ", Wrong Anwsers: " + this.incorrectAnswers);
+  }
+}
+
+
+/*
+        //Put your timeouts underneath this sentence
+        var fiveSecTimeout = setTimeout(fiveSeconds, 5000);
+        var tenSecTimeout = setTimeout(tenSeconds, 10000);
+        var timeUpTimeout = setTimeout(timeUp, 15000);
+
+      //Step 3:
+        //Fill in the blanks to these functions
+      function fiveSeconds() {
+        //Five seconds have passed, send an alert and state the time remaining in the "time-left" div.
+        alert("10 seconds remaining...");
+        $('#time-left').html("10 seconds remaining...");
+      }
+
+      function tenSeconds() {
+        //Ten seconds have passed, send an alert and state the time remaining in the "time-left" div.
+         alert("5 seconds remaining...");
+        $('#time-left').html("5 seconds remaining...");       
+      }
+
+      function timeUp(){
+        //Fifteen seconds have passed, time is up! Send an alert and have the "time-left" div let the user know.
+        alert("0 seconds remaining...");
+        $('#time-left').html("0 seconds remaining...");       
+
+        //The following line will play that audio file that you linked to above.
+        audio.play();
+      }
+*/
+
+/****
+ * FUNCTIONS
+ ****/
+
+function setTimeLeft(){
+  trivia.setTimeLeft();
+}
+
+function gameOver(){
+  trivia.gameOver();
+}
+
+$(document).ready(function() {
+  console.log("Basic Trivia Game");
+
+    $("#start-button").on("click", function() {
+      trivia.initGame();
+      trivia.start();
+    });
+});
